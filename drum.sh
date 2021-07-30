758151ce48565a25dd0a969a13d74e423f7a0051
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-07-30 10:16:22 -0900

    Update drum.sh

--- drum.sh
+++ drum.sh
@@ -4,5 +4,8 @@ while IFS= read -r line
 do
   inp="${line#[$'\r\t\n ']}"
   inp="${inp%[$'\r\t\n ']}"
+  if [[ ! $inp == "~/drum_bash/plugins/" ]]
+  then
   echo "~/drum_bash/plugins/$inp"
+  fi
 done < "$input"
