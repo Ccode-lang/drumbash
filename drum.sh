d77d048ed9b2877fda8bb1e51747012115c67ab3
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-07-30 13:52:15 -0900

    Update drum.sh

--- drum.sh
+++ drum.sh
@@ -11,3 +11,15 @@ do
   chmod +x plugins/$inp$plugin 2> /dev/null && plugins/$inp$plugin || echo "skipping plugin $inp$plugin because it does not exist."
   fi
 done < "$input"
+run=1
+while [ $run == 1 ]
+do
+printf "$"
+read com
+if [ $com == "exit" ]
+then
+  run=0
+else
+  "$com"
+fi
+done
