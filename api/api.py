6a47ea200f747b4e764884fdd57885baac146ed7
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-08-30 19:34:59 -0700

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,26 +1,21 @@
 from os import path
 
-
-#get drum api version
-def drumver():
-  return "drum bash py api 0.0.1"
-
-
-
-# create a file. returns int as error code
-def createfile(create):
-  try:
-    f = open(create, "x")
-    f.close()
-    # return 0 for no error
-    return 0
-  except:
-    # return 1 for error
-    return 1
-
-
-
-
-# check if a file exists. reurns bool
-def filecheck(check):
-  return path.exists(check)
+class file():
+  #get drum api version
+  def drumver():
+    return "drum bash py api 0.0.1"
+  
+  # create a file. returns int as error code
+  def create(create):
+    try:
+      f = open(create, "x")
+      f.close()
+      # return 0 for no error
+      return 0
+    except:
+     # return 1 for error
+     return 1
+  
+  # check if a file exists. reurns bool
+  def check(check):
+    return path.exists(check)
