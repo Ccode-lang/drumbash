7578989746486dc8bed8857734be5084e5280362
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-09-12 14:58:42 -0700

    Update drum.sh

--- drum.sh
+++ drum.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+# setup
 cd ~/drumbash
 mkdir bin
 plugin=".drum"
 input="drumrc"
+
+# main loop
 while IFS= read -r line
 do
+  #unescape
   inp="${line#[$'\r\t\n ']}"
   inp="${inp%[$'\r\t\n ']}"
+  #only init if drumrc line has content
   if [[ ! $inp == "" ]]
   then
   if [[ -f "plugins/$inp$plugin" ]]
   then
+    #make bin
     cp "plugins/$inp$plugin" bin
     cd bin
     mv "$inp$plugin" "$inp"
@@ -22,8 +28,11 @@ do
   fi
 done < "$input"
 
+# shell settings
 PATH="~/drumbash/bin:$PATH"
 run=1
+
+#shell loop
 while [[ $run == 1 ]]
 do
 printf "$ "
@@ -35,5 +44,7 @@ else
   eval "$com"
 fi
 done
+
+#finish
 cd ~/drumbash
 rm -r bin
